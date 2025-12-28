808e4da225ee5fa65a0d797c6430efc56f1be001
Lukáš Loukota <lukasloukota94@gmail.com>
2025-12-28 15:37:54 +0100

    test pr

--- packages/better-review/src/gh/gh.ts
+++ packages/better-review/src/gh/gh.ts
@@ -1,5 +1,17 @@
 import { Context, Effect } from "effect";
+import { Command } from "@effect/platform";
+import { BunCommandExecutor, BunRuntime } from "@effect/platform-bun";
+
+const getDiff = Effect.fn("ghGetDiff")(function* (pr: string) {
+  Command.make("gh pr diff" + pr);
+});
 
 interface GhCli {}
 
 class GhService extends Context.Tag("GHService")<GhService, GhCli>() {}
+
+const program = Effect.gen(function* () {
+  const gh = yield* GhService;
+});
+
+BunRuntime.runMain();
